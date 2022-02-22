8eb8a32c161fb4baa60d73d889ccc68426175805
Roberto Eustaquio <robertoeb.jr@gmail.com>
2022-02-22 18:09:35 -0300

    Add client register form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BsDropdownConfig, BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';;
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
-  providers: [],
+  providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true }}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
