f6e9011bfcc277d274ba94a87aa0b68f246b094d
Roberto Eustaquio <robertoeb.jr@gmail.com>
2022-02-22 21:45:21 -0300

    add authenticated routes, new components and new visual theme

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,21 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { BsDropdownConfig, BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';;
-import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component'
-
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component'
+import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -25,9 +32,9 @@ import { RegisterComponent } from './register/register.component'
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
   ],
-  providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true }}],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
@@ -10,18 +12,19 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
-    }, error => console.log(error))
+      this.router.navigateByUrl('/members');
+    }, error => this.toastr.error(error.error.title || error.error))
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
